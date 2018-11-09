eb635ecaac0ece16edf471301c0e6e25ffd694ec
YATSUO Hidenari <yatsuo@penguins.co.jp>
2018-11-09 14:40:54 +0900

    examples削除

--- examples/ner/config.jl
+++
@@ -1,9 +0,0 @@
-CONFIG = Dict()
-CONFIG["wordvec_file"] = ".data/glove.6B.100d.h5"
-CONFIG["train_file"] = ".data/eng.train.BIOES"
-CONFIG["dev_file"] = ".data/eng.testa.BIOES"
-CONFIG["test_file"] = ".data/eng.testb.BIOES"
-CONFIG["nepochs"] = 50
-CONFIG["learning_rate"] = 0.005
-CONFIG["batchsize"] = 10
-CONFIG["training"] = true
